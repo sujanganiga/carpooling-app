548eb6470f7dbe099965c0326e8ba3c91bc9a7bc
SUJAN GANIGA <sujan.ganiga2020@gmail.com>
2025-06-03 11:43:48 +0530

    kannda updated

--- server/config/database.js
+++ server/config/database.js
@@ -3,15 +3,11 @@ require("dotenv").config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
-  logging: false,
   dialectOptions: {
-    ssl:
-      process.env.NODE_ENV === "production"
-        ? {
-            require: true,
-            rejectUnauthorized: false,
-          }
-        : false,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
 });
 
